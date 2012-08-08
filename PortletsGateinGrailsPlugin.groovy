01dbf727b0655bafbdfee61b0eeab3659b3844eb
thomasdelhomenie <thomas.delhomenie@gmail.com>
2012-08-08 10:07:18 +0200

    Grails 2 support

--- PortletsGateinGrailsPlugin.groovy
+++ PortletsGateinGrailsPlugin.groovy
@@ -1,16 +1,16 @@
 class PortletsGateinGrailsPlugin {
 
    // the plugin version
-   def version = "0.2"
+   def version = "0.3"
    
    // the version or versions of Grails the plugin is designed for
-   def grailsVersion = "1.3.7 > *"
+   def grailsVersion = "2.0 > *"
    
    // the other plugins this plugin depends on
    def loadAfter = ['controllers']
    
    // the other plugins this plugin depends on
-    def dependsOn = [portlets:"0.8 > * "]
+    def dependsOn = [portlets:"0.9.2 > * "]
 
    // resources that are excluded from plugin packaging
    def pluginExcludes = [
